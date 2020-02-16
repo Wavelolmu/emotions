f500b80cabe3f2d768ea5b080b8d5d6e2fa5b13e
Vitaly Gachkovsky <wavelolmu@gmail.com>
2020-02-16 22:41:03 +0300

    Index.html problem solved

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ function starterTransition() {
   document.getElementById('transitionBtn1').style.display = 'none';
   document.getElementById('transitionBtn2').style.display = 'none';
   document.getElementById('starterScreen').style.opacity = 0;
-  setTimeout(function() { window.location.href = "../first_emotion.html"; }, 350);
+  setTimeout(function() { window.location.href = "first_emotion.html"; }, 350);
 }
